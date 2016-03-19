3b141e7a1c55dc45c4dc3dacf120b0a9667c96c6
Mikhail Tukai <tukaym@gmail.com>
2016-03-19 19:38:40 +0300

    created main menu

---
+++ src/js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$(".top-menu-toggle").click(function() {
+		$(this).toggleClass("on");
+		$(".top-menu-main").slideToggle();
+		return false;
+	});
+
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$(".forms").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.magnificPopup.close();
+				$(".forms").trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
