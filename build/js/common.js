70f208df073d4185f6256b94ef3cc380699d4f04
Mikhail Tukai <tukaym@gmail.com>
2016-03-22 23:31:48 +0300

    added portfolio section, need to fix filters

--- build/js/common.js
+++ build/js/common.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     responsive:{
         600:{
             items:3,
-            nav:false
+        
         },
         1000:{
             items:3,
@@ -29,6 +29,18 @@ $(document).ready(function() {
         }
       }
 });
+$('.portfolio-carousel').owlCarousel({
+	  loop: true,
+    nav: false,
+    margin:0,
+    dots: true,
+    animateIn: true,
+    animateOut: true,
+    items: 3,
+    center: true,
+    autoWidth: false,
+  });
+
 
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
@@ -47,7 +59,58 @@ $(document).ready(function() {
 		return false;
 	});
 
-
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
 });
+
+// portfolio filter
+var itemsToFilter = document.querySelectorAll('.portfolio-carousel img');
+var checkBoxes = document.querySelectorAll('.filters input');
+for (var i = 0; i<checkBoxes.length; i++) {
+	checkBoxes[i].addEventListener('click', filterItems, false);
+	checkBoxes[i].checked = true;
+}
+
+function filterItems(e) {
+	var clickedItem = e.target;
+
+	if(clickedItem.checked == true) {
+		hideOrShowItems(clickedItem.value, 'hideItem', 'showItem');
+	} else if (clickedItem.checked == false) {
+		hideOrShowItems(clickedItem.value, 'showItem', 'hideItem');
+	}
+}
+
+function hideOrShowItems(itemType, classToRemove, classToAdd) {
+	for (var i = 0; i < itemsToFilter.length; i++) {
+		var currentItem = itemsToFilter[i];
+		if (currentItem.getAttribute('data-type') == itemType) {
+			removeClass(currentItem, classToRemove);
+			addClass(currentItem, classToAdd);
+		}
+	}
+}
+function addClass(element, classToAdd) {
+	var currentClassValue = element.className;
+	if (currentClassValue.indexOf(classToAdd) == -1) {
+		if ((currentClassValue == null) || (currentClassValue === '')) {
+			element.className = classToAdd;
+	} else {
+		element.className += '' + classToAdd;
+	}
+}
+}
+function removeClass(element, classToRemove) {
+	var currentClassValue = element.className;
+	if (currentClassValue == classToRemove) {
+		element.className = '';
+		return;
+	}
+	var classValues = currentClassValue.split('');
+	var filteredList = [];
+	for (var i = 0; i < classValues.length; i++) {
+		if (classToRemove != classValues[i]) {
+			filteredList.push(classValues[i]);
+		}
+	}
+	element.className = filteredList.join(' ');
+}
