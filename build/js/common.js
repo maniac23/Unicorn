51c8ea6bcd16713f3559250e9ea815f89ac761ce
Mikhail Tukai <tukaym@gmail.com>
2016-03-28 19:33:09 +0300

    added underline mixin

--- build/js/common.js
+++ build/js/common.js
@@ -5,27 +5,7 @@ $(document).ready(function() {
 		$(".top-menu-main").slideToggle();
 		return false;
 	});
-
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$(".forms").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.magnificPopup.close();
-				$(".forms").trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 });
-
 // portfolio filter
 var itemsToFilter = document.querySelectorAll('.portfolio-slider img');
 var checkBoxes = document.querySelectorAll('.filters input');
